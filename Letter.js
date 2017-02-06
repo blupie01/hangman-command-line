4a73b4e882b2a2bd1abde84a671596420e12cb49
blupie01 <jeffrey.chou.89@gmail.com>
2017-02-06 16:28:11 -0500

    added colors npm package

--- Letter.js
+++ Letter.js
@@ -1,20 +1,6 @@
 // `Letter.js` should control whether or not a letter 
 // appears as a "_" or as itself on-screen.
 
-// function 
 
-// var Word = require("./Word");
-
-// var selectedWord = Word.wordSelected;
-
-// var dashedWord = "";
-
-// console.log(selectedWord);
-
-// while (dashedWord.length < selectedWord.length * 2) {
-// 	dashedWord += "_ ";
-// }
-
-// console.log(dashedWord);
 
 // module.exports = Letter;
--- game.js
+++ game.js
@@ -2,8 +2,12 @@ var inquirer = require("inquirer");
 var Letter = require("./Letter");
 var Word = require("./Word");
 
-var lives = 8;
+var lives = 10;
 var dashedWord = "";
+
+console.log("Mythical Creatures Hangman!");
+console.log("Instructions: Guess a letter until you complete the word or lose.");
+console.log("--------------------------------------------------------------------");
 
 var wordArray = [
 	"Cats",
@@ -25,4 +29,15 @@ while (dashedWord.length < word.length * 2) {
 console.log(word);
 console.log(dashedWord);
 
-// inquirer.prompt()
+//----------FUNCTIONS------------------------------------------------
+
+function guessLetter(){
+	inquirer.prompt([
+	  {type: "input",
+	    name: "letter",
+	    message: "What's your letter?"}
+
+	]).then(function(data){
+	      var letter = 
+	});
+}
